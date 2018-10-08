a4fd3de5e7465ec747373cfca9191835a9594b9a
Scior <interval12@gmail.com>
2018-10-08 22:53:47 +0900

    Initial Commit

---
+++ Shallux/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shallux
+//
+//  Created by Suita Fujino on 2018/10/08.
+//  Copyright © 2018 Suita Fujino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
